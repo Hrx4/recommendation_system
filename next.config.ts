39e4d180f6789b34d4c06a5013d11fcc19477fa5
Kamal <dragkamal71@gmail.com>
2024-10-28 12:02:54 +0530

    cors changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers(){
+    return [
+      {
+        source: "/api/:path*",
+        headers:[
+          {
+            key:"Access-Control-Allow-Origin", value:"*"
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
